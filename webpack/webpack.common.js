00a7bef166cf19209e2a18c2712726ecb12bd491
Federico Kauffman <federico@streaver.com>
2022-12-08 19:37:59 -0300

    Simplify build

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -12,14 +12,6 @@ module.exports = {
     path: path.join(__dirname, "../dist/js"),
     filename: "[name].js",
   },
-  optimization: {
-    splitChunks: {
-      name: "vendor",
-      chunks(chunk) {
-        return chunk.name !== "background";
-      },
-    },
-  },
   module: {
     rules: [
       {
